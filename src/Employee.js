65091d8b7aabe22ee8691b948f5ca78328f5498c
prathijna <prathijnads2216@gmail.com>
2024-06-04 11:38:59 +0530

    devops initail commit

---
+++ src/Employee.js
@@ -0,0 +1,7 @@
+function Employee () {
+    return(
+        <div>Hii Employee Here!</div>
+    );
+}
+
+export default Employee;
---
+++ src/POST.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react';
+
+function POST() {
+ const [state,setPost]=useState();
+ const loadPost = () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(Response => Response.json())
+    .then(res => setPost(res))
+ }
+  return (
+    <div>POST
+
+        {
+            state ? <p>id : {state.id}</p> : null
+        }
+        <button onClick={loadPost}>Get Post</button>
+    </div>
+  )
+}
+
+export default POST
